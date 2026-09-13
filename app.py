23c3018c4a46bd78331fc65f8b904639d1b184e5
sameer25bai10985-alt <sameer.25bai10985@vitbhopal.ac.in>
2026-09-13 20:51:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
-from routes.subject_routes import attendance_bp as subjects_bp
+
+from routes.subject_routes import subject_bp
 from routes.attendance_routes import attendance_bp
+
+
 app = Flask(__name__)
+
+app.register_blueprint(subject_bp)
 app.register_blueprint(attendance_bp)
-app.register_blueprint(subjects_bp)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
