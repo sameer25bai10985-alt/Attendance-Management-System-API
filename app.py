b21a58c0b7d84ec92e53d670b823dda53f9eedf6
sameer25bai10985-alt <sameer.25bai10985@vitbhopal.ac.in>
2026-09-13 20:57:36 +0530

    Register student_routes blueprint in app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 
 from routes.subject_routes import subject_bp
 from routes.attendance_routes import attendance_bp
+from routes.student_routes import student_bp
 
 
 app = Flask(__name__)
 
 app.register_blueprint(subject_bp)
 app.register_blueprint(attendance_bp)
+app.register_blueprint(student_bp)
 
 
 if __name__ == '__main__':
